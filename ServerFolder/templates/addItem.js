0b08b6d2abf04a7fb3d01733267c54c9f7e410f4
harbm17 <harbm17@students.ecu.edu>
2021-11-07 13:08:51 -0500

    added button to add books to liked books

--- ServerFolder/templates/addItem.js
+++ ServerFolder/templates/addItem.js
@@ -1,46 +1,8 @@
-const toggleBtn = document.querySelector('#toggleBtn');
-const divList = document.querySelector('.booksLiked');
-
-toggleBtn.addEventListener('click', () => {
-    if(divList.style.display == 'none')
-    {
-        divList.style.display = 'block';
-        toggleBtn.innerHTML = 'Hide List';
-    }
-    else
-    {
-        divList.style.display = 'none';
-        toggleBtn.innerHTML = 'Show List';
-    }
-});
-
-/*add list item*/
-const addBook = document.querySelector('#addBook');
-const addBtn = document.querySelector('#addBtn');
-
-function addLists() {
-    if (addInput.value === '') {
-      alert('Enter the book please!!!');
-    } else {
-      const ul = divList.querySelector('ul');
-      const li = document.createElement('li');
-      li.innerHTML = addInput.value;
-      addInput.value = '';
-      ul.appendChild(li);
-      createBtn(li);
-    }
-  }
-
-addBtn.addEventListener('click', ()=> {
-    addLists();
-});
-
-addInput.addEventListener('keyup', (event) => {
-    if(event.which === 13) {
-      addLists();
-    }
-  });
-
-for (var i = 0; i < lis.length; i++) {
-    createBtn(lis[i]);
+function addBook(){
+  var ul = document.getElementById("bookList");
+  var book = document.getElementById("book");
+  var li = document.createElement("li");
+  li.setAttribute('id', book.value);
+  li.appendChild(document.createTextNode(book.value));
+  ul.appendChild(li);
 }
