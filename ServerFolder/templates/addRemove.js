c6f4e8e184a22c2f8929847bec3549a617c9956c
harbm17 <harbm17@students.ecu.edu>
2021-11-09 10:22:19 -0500

    added author likes

---
+++ ServerFolder/templates/addRemove.js
@@ -0,0 +1,31 @@
+function addBook(){
+  var ul = document.getElementById("bookList");
+  var book = document.getElementById("book");
+  var li = document.createElement("li");
+  li.setAttribute('id', book.value);
+  li.appendChild(document.createTextNode(book.value));
+  ul.appendChild(li);
+}
+
+function removeBook(){
+  var ul = document.getElementById("bookList");
+  var book = document.getElementById("book");
+  var li = document.getElementById(book.value);
+  ul.removeChild(li);
+}
+
+function addAuthor(){
+  var ul = document.getElementById("authorList");
+  var author = document.getElementById("author");
+  var li = document.createElement("li");
+  li.setAttribute('id', author.value);
+  li.appendChild(document.createTextNode(author.value));
+  ul.appendChild(li);
+}
+
+function removeAuthor(){
+  var ul = document.getElementById("authorList");
+  var author = document.getElementById("author");
+  var li = document.getElementById(author.value);
+  ul.removeChild(li);
+}
